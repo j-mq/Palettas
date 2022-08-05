42c2fb37803d75892f2c454edbe96f67cd999453
Jacobo <jacobo@fixel.co.jp>
2022-08-05 15:46:45 +0900

    Vue setup, main layout

---
+++ palettas/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div class="layoutContainer">
+      <ColorSampling />
+    </div>
+  </div>
+</template>
+
+<script>
+import ColorSampling from "./components/ColorSampling.vue";
+export default {
+  name: "App",
+  components: { ColorSampling },
+};
+</script>
+
+<style>
+body {
+  background-color: #808080;
+  margin: 0px;
+  padding: 0px;
+}
+div {
+  box-sizing: border-box;
+}
+.container {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+}
+.layoutContainer {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 350px;
+}
+</style>
---
+++ palettas/src/components/ColorSampling.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="colorSampling"></div>
+</template>
+
+<script>
+export default {
+  name: "ColorSampling",
+  props: {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.colorSampling {
+  width: 100%;
+  background: #b4b4b4;
+  height: 250px;
+  border-radius: 4px;
+}
+</style>
